2becb93c4868a9bebc55edd179c547dbf643c8e0
Markus Gilg <markusgilg@outlook.de>
2020-07-14 21:47:17 +0200

    export

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 const {ReferenceSystem, ReferenceSystemProperties} = require('./reference_system/reference.system');
 const {Point} = require('./geometry/point');
+const {MultiPoint} = require('./geometry/multi.point');
+const {Line} = require('./geometry/line');
+const {MultiLine} = require('./geometry/multi.line');
+const {Polygon} = require('./geometry/polygon');
+const {MultiPolygon} = require('./geometry/multi.polygon');
+const {Feature} = require('./geometry/feature');
+const {FeatureCollection} = require('./geometry/feature.collection');
 
 module.exports = {
     ReferenceSystem, ReferenceSystemProperties,
-    Point,
+    Point, MultiPoint,
+    Line, MultiLine,
+    Polygon, MultiPolygon,
+    Feature, FeatureCollection,
 };
